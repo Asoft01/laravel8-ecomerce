5d8b1f1b3bd40d1b8dbb7a5dff3af72fcfdfdb89
Adeleke Hammed <lekhad19@gmail.com>
2021-07-03 21:12:19 +0100

    Query Builder Read Users Data

---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Http\Controllers\ContactController;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/home', function () {
+    echo " This is Home page ";
+});
+
+Route::get('/about', function(){
+    return view('about');
+});
+
+// Route::get('/about', function () {
+//     return view('about');
+// })->middleware('check');
+
+
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
+
+// Route::get('/contact', [ContactController::class, 'index']);
+Route::get('/contact-sdbjksd-sdhbh', [ContactController::class, 'index'])->name('ariyan');
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    // $users = User::all();
+
+    // Using Query Builder
+    $users= DB::table('users')->get();
+    
+    return view('dashboard', compact('users'));
+})->name('dashboard');
